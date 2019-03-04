d87bcc567e38db505e701f4c60d31d557a688abe
saekishin <43920663+saekishin@users.noreply.github.com>
2019-03-05 08:53:10 +0900

    Update main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+//aa
+  var vm = new Vue({
+//aa
+   el: '#app',
+//aa
+    data: {
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3'
+      ]
+    }
+  });
+})();
