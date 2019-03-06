cd76d37f966d1cb018e495a95f51284210f13937
saekishin <43920663+saekishin@users.noreply.github.com>
2019-03-06 09:04:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,26 @@
         'task 2',
         'task 3'
       ]
+    },
+    
+    methods: {
+      //eはなんだ？　preventDfaultが無くてもできるのか？　
+      //addItem: function(e) {
+      //e.preventDefault();
+      //this.todos.push(this.newItem);
+      
+      
+    addItem: function() {
+//this：このドキュメント push：追加する this.newItem：これはhtmlで指定した変数？
+      this.todos.push(this.newItem);
+//テキストボックスの中身を空にしてあげる
+      this.newItem = '';
     }
+    
+    //methodsの閉じ
+    }
+//new Vueの閉じ
   });
+  
+//最初のfunctionの閉じ
 })();
