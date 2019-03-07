e4c9f9ab6c359fbe60ff83c25686b44ec4e70d3e
saekishin <43920663+saekishin@users.noreply.github.com>
2019-03-07 09:19:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//いったんコードを模写して、動画をみながらコメントを入れていく
 (function() {
   'use strict';
 
@@ -7,7 +8,8 @@
    el: '#app',
 
     data: {
-//newItemが定義されてなかった
+//newItemが定義されてなかった。空欄にしとく
+      newItem: '',
       todos: [
         'task 1',
         'task 2',
@@ -23,14 +25,30 @@
       
       
     addItem: function() {
+      
 //this：このドキュメント push：追加する this.newItem：これはhtmlで指定した変数？
       this.todos.push(this.newItem);
+      
 //テキストボックスの中身を空にしてあげる
       this.newItem = '';
-    }
+    },
+   //↑メソッドを区切るためにカンマを入れている
+   
     
-    //methodsの閉じ
+   //↓配列のindexを指定するアクション
+    deleteItem: function(index) {
+      
+   //confirmで確認ポップアップを出現させる（コメントは自由に設定できる）※ポップアップの見た目もいじる方法がある。
+      if (confirm('are you sure?')) {
+   //spliceで削除できる　1ってなんだっけ？
+        this.todos.splice(index.1); 
+      }
+      
+   //↓deleteItemの閉じ
     }
+      
+    //methodsの閉じ
+  }
 //new Vueの閉じ
   });
   
